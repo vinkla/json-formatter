f1b6f60a02c29b371dbcc6678134ad86f8b0d1e1
Vincent Klaiber <vincentklaiber@gmail.com>
2018-12-03 20:13:28 +0100

    Update if statement

--- src/index.js
+++ src/index.js
@@ -7,20 +7,18 @@ const stylesheet = `
 
 document.addEventListener('DOMContentLoaded', () => {
   if (
-    !document.body ||
-    document.body.children.length !== 1 ||
-    document.body.children[0].tagName !== 'PRE'
+    document.body &&
+    document.body.children.length === 1 &&
+    document.body.children[0].tagName === 'PRE'
   ) {
-    return;
+    document.body.normalize();
+    const content = document.body.textContent.trim();
+    const json = JSON.stringify(JSON.parse(content), null, 2);
+    const data = linkifyUrls(json);
+    document.body.innerHTML = `<pre>${data}</pre>`;
+
+    const style = document.createElement('style');
+    style.textContent = stylesheet;
+    document.head.appendChild(style);
   }
-
-  document.body.normalize();
-  const content = document.body.textContent.trim();
-  const json = JSON.stringify(JSON.parse(content), null, 2);
-  const data = linkifyUrls(json);
-  document.body.innerHTML = `<pre>${data}</pre>`;
-
-  const style = document.createElement('style');
-  style.textContent = stylesheet;
-  document.head.appendChild(style);
 });
